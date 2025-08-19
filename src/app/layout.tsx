9350dd4ab316e679bc580e3aed46b3b9e9ab3812
FikFikk <fikri225456@gmail.com>
2025-08-19 11:38:35 +0700

    Initial Commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
+import Script from "next/script";
+// import "./globals.css";
+import  "bootstrap/dist/css/bootstrap.min.css"
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -24,9 +26,17 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        
+      </head>
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
         {children}
       </body>
+      
+
     </html>
+    
   );
 }
+
+
